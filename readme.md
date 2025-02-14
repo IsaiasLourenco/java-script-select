02cf644e0c7b1f57cdb21e1029fae46a35467929
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-02-14 19:45:55 -0300

    First Commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Projeto simples em JavaScript com select option
+Funcionalidade simples feito em JavaSript, CSS e HTML
+
+## Functionalities
+
+Pega através do id="option" do select, o valor que é passado pelo select e usa isso para decidir de que maneira vai apresentar a mensagem "Bem vindo Isaias!"
+
+## Tecnologias utilizadas
+
+- JavaScript; 
+HTML;
+- CSS.
+
+## Initialization
+
+Não há necessidade de nenhum npm ao executar o git.clone.
+
+### Colaboradores
+
+- Isaias Lourenço. 'https://github.com/IsaiasLourenco/'.
+
+[<img src="./src/js_moves_html_css.gif" alt="JavaScript">]
---
+++ src/main.js
@@ -0,0 +1,33 @@
+document.getElementById("option").addEventListener("change", function() {
+    let selectedOption = this.value; // Captura a opção selecionada
+    hello("Isaias", selectedOption); // Chama a função com a opção correta
+});
+
+function hello(name, type) {
+    let welcome = `Bem-vindo ${name}!`;
+    write(welcome, type);
+}
+
+function write(phrase, type) {
+    let outputElement = document.getElementById("output");
+
+    if (type !== "console") {
+        console.clear(); // Limpa o console se a opção não for "console"
+    }
+
+    switch(type) {
+        case "console":
+            console.log(phrase);
+            outputElement.innerText = ""; // Apaga o texto da página
+            break;
+        case "alert":
+            alert(phrase);
+            outputElement.innerText = ""; // Apaga o texto da página
+            break;
+        case "page":
+            outputElement.innerText = phrase; // Exibe na página
+            break;
+        default:
+            console.warn("Nenhuma opção válida selecionada.");
+    }
+}
